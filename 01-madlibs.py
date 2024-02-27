ba2083390aefa8df5c1f6107ff0418a3ce6d66f5
MHEshrak <mahadihasaneshrak@gmail.com>
2024-02-27 16:07:38 +0600

    Create 01-madlibs.py

---
+++ 01-madlibs.py
@@ -0,0 +1,9 @@
+adjective = input("adjective: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+famous_person_name = input("famous person name: ")
+
+madlibs = f"Computer programming is a {adjective} things. Because i love to {verb1}.\
+Stay hydrated and healthy and {verb2} like you are {famous_person_name}."
+
+print(madlibs)
